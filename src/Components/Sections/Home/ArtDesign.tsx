5ce9e5fabc9c9648b61b87352daac21d3e5115cf
Leonardo Belilo <leonardoBelilo@outlook.com>
2023-02-04 21:09:12 -0300

    edit art e design

--- src/Components/Sections/Home/ArtDesign.tsx
+++ src/Components/Sections/Home/ArtDesign.tsx
@@ -8,14 +8,14 @@ export function ArtDesingn(){
                 Art&Design 
             </Text>
             <Stack pt='12' gap={'8'} direction={'row'}>
-                <Box rounded={"2xl"} h='20rem' bg='gray.800' w='20rem'></Box>
-                <Box rounded={"2xl"}  h='20rem' bg='gray.800' w='20rem'></Box>
-                <Box rounded={"2xl"}h='20rem' bg='gray.800' w='20rem'></Box>
+                <Box rounded={"2xl"} h='25rem' bg='gray.800' w='25rem'></Box>
+                <Box rounded={"2xl"}  h='25rem' bg='gray.800' w='25rem'></Box>
+                <Box rounded={"2xl"}h='25rem' bg='gray.800' w='25rem'></Box>
             </Stack>
             <Stack pt='12' gap={'8'} direction={'row'}>
-                <Box rounded={"2xl"} h='20rem' bg='gray.800' w='20rem'></Box>
-                <Box rounded={"2xl"}  h='20rem' bg='gray.800' w='20rem'></Box>
-                <Box rounded={"2xl"}h='20rem' bg='gray.800' w='20rem'></Box>
+                <Box rounded={"2xl"} h='25rem' bg='gray.800' w='25rem'></Box>
+                <Box rounded={"2xl"}  h='25rem' bg='gray.800' w='25rem'></Box>
+                <Box rounded={"2xl"}h='25rem' bg='gray.800' w='25rem'></Box>
             </Stack>
         </VStack>
         </>
